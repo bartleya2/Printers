f9d96843b90e143d220f29bda4f6468a6361f684
Anthony Bartley <anthonybartley@Anthonys-iMac.local>
2021-10-19 18:33:40 -0400

    First Commit

---
+++ src/config.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
+const description =
+  "This is the description of your NFT project, remember to replace this";
+const baseUri = "ipfs://NewUriToReplace";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 3333,
+    layersOrder: [
+      { name: "Backgrounds" },
+      { name: "Printers" },
+      { name: "Messages" },
+      { name: "Assets" },
+      { name: "Lights" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 3000,
+  height: 3000,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.width / format.height,
+  imageName: "preview.png",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+};
